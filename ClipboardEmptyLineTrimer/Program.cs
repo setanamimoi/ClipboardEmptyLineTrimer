3b9bb7e82e15973fa112aee3606418e98f719337
setanamimoi <setanamimoi@gmail.com>
2012-08-25 09:34:04 +0900

    正常系処理を追加

--- ClipboardEmptyLineTrimer/Program.cs
+++ ClipboardEmptyLineTrimer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ClipboardEmptyLineTrimer
 {
@@ -15,6 +16,22 @@ namespace ClipboardEmptyLineTrimer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            using(StringWriter writer = new StringWriter())
+            using(StringReader reader = new StringReader(Clipboard.GetText()))
+            {
+                for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    if (line.Any() == false)
+                    {
+                        continue;
+                    }
+
+                    writer.WriteLine(line);
+                }
+
+                Clipboard.SetText(writer.ToString());
+            }
         }
     }
 }
